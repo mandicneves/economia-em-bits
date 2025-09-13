c2954998ad565c529d3c01204a07a5e252176efb
Marcelo Neves Lira <78197411+mandicneves@users.noreply.github.com>
2025-09-13 04:47:01 +0000

    Building first steps of portfolio

--- src/portfolio/models.py
+++ src/portfolio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    methodology = models.TextField(blank=True, null=True)
+    results = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="portfolio/", blank=True, null=True)
+    github_link = models.URLField(blank=True, null=True)
+    demo_link = models.URLField(blank=True, null=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+    
+    def __str__(self):
+        return self.title
--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 # Create your views here.
+def portfolio_list(request, *args, **kwargs):
+    projects = Project.objects.all()
+    return render(request, "portfolio/portfolio_list.html", {})
+
+def portfolio_detail(request, slug, *args, **kwargs):
+    project = get_object_or_404(Project, slug=slug)
+    return render(request, "portfolio/portfolio_detail.html", {})
